57c06932cc06c39d98941c49a2212038e9abed37
ittarvin <wangpfitta@163.com>
2015-04-17 11:37:37 +0800

    冒泡排序-添加注释

--- sort/src/sort/bubble/Sort.java
+++ sort/src/sort/bubble/Sort.java
@@ -10,15 +10,17 @@ public class Sort {
 	public static void main(String args[]) {
 
 		int[] list = { 1, 3, 23, 4, 23, 23, 232, 2, 22, 1 };
-		
+
 		boolean needNextPass = true;
 
+		// 比较次数为 (list size - 1)
 		for (int k = 1; k < list.length && needNextPass; k++) {
-			
+
 			needNextPass = false;
-			
+
+			//相邻的二个元素做比较
 			for (int i = 0; i < list.length - k; i++) {
-				//swap list[i] with list[i+1]
+				// swap list[i] with list[i+1]
 				if (list[i] > list[i + 1]) {
 					int temp = list[i];
 					list[i] = list[i + 1];
@@ -26,12 +28,12 @@ public class Sort {
 					needNextPass = true;
 				}
 			}
-			
+
 		}
 
 		for (int i = 0; i < list.length; i++) {
 			System.out.println(list[i]);
 		}
-		
+
 	}
 }
