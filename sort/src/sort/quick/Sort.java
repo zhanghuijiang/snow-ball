1175c7648e28046a0847eefad64d4ff624d3abeb
ittarvin <wangpfitta@163.com>
2015-04-17 21:20:10 +0800

    快速排序-添加注释

--- sort/src/sort/quick/Sort.java
+++ sort/src/sort/quick/Sort.java
@@ -15,26 +15,30 @@ public class Sort {
 		
 		while(high > low){
 			
+			//由数组的左边向右边查找出第一个 > pivot 的数组值的下标
 			while(low <=high && list[low] <= pivot){
 				low ++ ;
 			}
 			
+			//由数组的右边向左边查找出第一个 <= pivot 的数组值的下标
 			while(low <=high && list[high] > pivot){
 				high --;
 			}
 			
+			//将查找出的两个数组值交换位置
 			if(high > low){
 				int temp = list[high];
 				list[high] = list[low];
 				list[low] = temp;
 			}
-			
 		}
 		
+		//继续查找第一个 < pivot 的数组值
 		while(high > first && list[high] >= pivot){
 			high --;
 		}
 		
+		//确定 pivot 在数组中的位置
 		if(pivot > list[high]){
 			list[first] = list[high];
 			list[high] = pivot;
@@ -48,8 +52,11 @@ public class Sort {
 	
 	public static void sort(int[] list,int first,int last){
 		if(last>first){
+			//在数组中的位置
 			int pivotIndex = partition(list, first, last);
+			//排序左边的数组
 			sort(list, first, pivotIndex -1);
+			//排序右边边的数组
 			sort(list, pivotIndex + 1, last);
 		}
 	}
